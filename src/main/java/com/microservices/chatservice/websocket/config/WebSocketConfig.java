e95fd71f9add7437eb0cf5dd1fb4f450d163d379
NhatBangLe <91333510+NhatBangLe@users.noreply.github.com>
2024-09-19 17:56:30 +0700

    Remove CORS WebSocket

--- src/main/java/com/microservices/chatservice/websocket/config/WebSocketConfig.java
+++ src/main/java/com/microservices/chatservice/websocket/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.microservices.chatservice.websocket.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
@@ -10,9 +9,6 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocketMessageBroker
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-
-    @Value("${ALLOWED_ORIGIN}")
-    private String allowedOrigin;
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -23,8 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chat")
-                .setAllowedOrigins(allowedOrigin);
+        registry.addEndpoint("/chat");
     }
 
 }
